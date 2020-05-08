308d1cc11792b5f707cb6d7fd6e85623f0adce3a
Nicholas Yang <ny585@nyu.edu>
2020-05-08 02:00:13 -0400

    Starting to clean up file locations and names

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
+extern crate chrono;
 extern crate difference;
-extern crate tokio;
 extern crate serde_json;
+extern crate tokio;
 
+use chrono::Utc;
 use difference::{Changeset, Difference};
-use tokio::time;
-use std::time::Duration;
 use serde_json::json;
-use std::{fs};
+use std::{fs, io};
 use std::fs::OpenOptions;
 use std::io::Write;
-
+use std::time::Duration;
+use tokio::time;
 
 fn diffs_to_json(diffs: &Vec<Difference>) -> String {
     let mut values = Vec::new();
     for diff in diffs {
-        let (type_ , content)= match diff {
+        let (type_, content) = match diff {
             Difference::Add(c) => ("add", c),
             Difference::Rem(c) => ("remove", c),
-            Difference::Same(c) => ("same", c)
+            Difference::Same(c) => ("same", c),
         };
         let val = json!({
            "type": type_,
@@ -29,17 +30,23 @@ fn diffs_to_json(diffs: &Vec<Difference>) -> String {
 }
 
 #[tokio::main]
-async fn main() {
-    let mut interval = time::interval(Duration::from_millis(500));
-    let mut previous_contents = fs::read_to_string("test.js").expect("Something went wrong");
-    let mut out_file = OpenOptions::new().write(true).append(true).create(true).open("out").unwrap();
+async fn main() -> Result<(), io::Error> {
+    let mut interval = time::interval(Duration::from_millis(1000));
+    let mut previous_contents = fs::read_to_string("src/main.rs").expect("Something went wrong");
+    let out_filename = format!("outfiles/{}.json", Utc::now());
+    let mut out_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(out_filename)
+        .unwrap();
 
     loop {
         interval.tick().await;
-        let current_contents = fs::read_to_string("test.js").expect("Something went wrong");
+        let current_contents = fs::read_to_string("src/main.rs").expect("Something went wrong");
         let changeset = Changeset::new(&previous_contents, &current_contents, "\n");
         if changeset.distance > 0 {
-            writeln!(out_file, "{}", diffs_to_json(&changeset.diffs));
+            writeln!(out_file, "{}", diffs_to_json(&changeset.diffs))?;
         }
         previous_contents = current_contents;
     }

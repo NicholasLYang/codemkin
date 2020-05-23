9e296e68a4a9c792735bfb7a50474346364db686
Nicholas Yang <ny585@nyu.edu>
2020-05-23 00:59:06 -0400

    Trying to get history working

--- src/history.rs
+++ src/history.rs
@@ -1,7 +1,11 @@
 use crate::types::ChangeElement;
 use anyhow::Result;
 use rusqlite::{params, Connection};
+use std::io::stdin;
 use std::path::Path;
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 pub fn display_file_history(path: &Path, conn: &Connection) -> Result<()> {
     let mut changes_query = conn.prepare(
@@ -15,11 +19,29 @@ pub fn display_file_history(path: &Path, conn: &Connection) -> Result<()> {
         .query_map(params![path.canonicalize()?.to_str().unwrap()], |row| {
             Ok(row.get(0)?)
         })?;
+    let stdin = stdin();
+    let mut file_states = Vec::new();
     for change in changes {
         let elem_str: String = change?;
         let elements = serde_json::from_str::<Vec<ChangeElement>>(&elem_str)?;
-        for elem in elements {
-            print!("{}", elem.content);
+        let file = elements
+            .into_iter()
+            .map(|elem| elem.content)
+            .collect::<Vec<String>>()
+            .join("");
+        file_states.push(file);
+    }
+    let mut index: usize = 0;
+    print!("{}", file_states[index]);
+    for event in stdin.events() {
+        match event? {
+            Event::Key(Key::Char(' ')) => {
+                index += 1;
+                print!("\x1B[2J");
+                print!("{}", file_states[index]);
+                println!("{}", index);
+            }
+            _ => {}
         }
     }
     Ok(())
